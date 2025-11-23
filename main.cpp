6b049b7d4176979e8f67403e4e17442f538bf779
kjhccscrepo <j_kist1@mail.harpercollege.edu>
2025-11-23 10:26:36 -0600

    Made it so calc only does operations on Natural Numbers, Added test cases.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include "classes/TriangleNumberCalculator.hpp"
+
 int main() {
     std::cout << "Welcome to the triangle calculator program\n";
-    std::cout << TriangleNumberCalculator::value(9);
+    std::cout << TriangleNumberCalculator::value(1);
+    std::cout << "\n";
+    std::cout << TriangleNumberCalculator::value(2);
+    std::cout << "\n";
+    std::cout << TriangleNumberCalculator::value(4);
     std::cout << "\n";
     std::cout << TriangleNumberCalculator::value(3);
     std::cout << "\n";
+    std::cout << TriangleNumberCalculator::add(1, 1);
+    std::cout << "\n";
+    std::cout << TriangleNumberCalculator::add(2, 3);
+    std::cout << "\n";
+    std::cout << TriangleNumberCalculator::add(4, 2);
+    std::cout << "\n";
     std::cout << TriangleNumberCalculator::add(8, 2);
+    std::cout << "\n";
+    std::cout << TriangleNumberCalculator::subtract(1, 1);
+    std::cout << "\n";
+    std::cout << TriangleNumberCalculator::subtract(2, 3);
+    std::cout << "\n";
+    std::cout << TriangleNumberCalculator::subtract(4, 2);
     std::cout << "\n";
     std::cout << TriangleNumberCalculator::subtract(9, 3);
     std::cout << "\n";
